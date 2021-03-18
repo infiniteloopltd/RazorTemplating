e899147a0dcfbe72087cbd579ea6d5ba81e36d89
Fiach Reid <fiach@globaldataconsortium.com>
2021-03-18 10:29:34 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json.Linq;  // Install-Package Newtonsoft.JSON
+using RazorEngine; // Install-Package RazorEngine.NetCore
+using RazorEngine.Templating;
+
+namespace RazorDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = File.ReadAllText("data.json");
+            var template = File.ReadAllText("template.html");
+            dynamic model = JObject.Parse(data);
+            var result =
+                Engine.Razor.RunCompile(
+                    template,
+                    "template",
+                    null,
+                    (object)model);
+            Console.WriteLine(result);
+        }
+    }
+}
